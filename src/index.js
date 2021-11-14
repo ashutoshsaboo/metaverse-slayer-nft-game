56346239cf071ed92489f2d7074e88e570543dad
Ashutosh Saboo <ashsaboo@amazon.com>
2021-11-14 11:50:43 +0530

    Revert "test for deploying to subdir on gh pages"
    This reverts commit 7a171fe05c838478a75fefb926b93f8c5fff120d.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createBrowserHistory } from 'history';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-// export const history = createBrowserHistory({
-//   basename: process.env.PUBLIC_URL
-// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <Route path={`/`} component={App} />
-      {/* <App /> */}
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
