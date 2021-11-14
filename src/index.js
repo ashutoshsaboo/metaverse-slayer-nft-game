7a171fe05c838478a75fefb926b93f8c5fff120d
Ashutosh Saboo <ashsaboo@amazon.com>
2021-11-14 11:13:01 +0530

    test for deploying to subdir on gh pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// export const history = createBrowserHistory({
+//   basename: process.env.PUBLIC_URL
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <Route path={`/`} component={App} />
+      {/* <App /> */}
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
